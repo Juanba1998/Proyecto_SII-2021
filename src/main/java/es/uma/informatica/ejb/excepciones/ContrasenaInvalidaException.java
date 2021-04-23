b59bcbe640e5b48c6f496f12414a721301162354
juanba1998 <juanba98@uma.es>
2021-04-23 11:17:18 -0700

    Revert "Revert "update con mis ejb""
    This reverts commit b2266e5d1ab55da1bb640fe3a768c33edc60ae7e.

--- src/main/java/es/uma/informatica/ejb/excepciones/ContrasenaInvalidaException.java
+++ src/main/java/es/uma/informatica/ejb/excepciones/ContrasenaInvalidaException.java
@@ -1,5 +1,6 @@
 package es.uma.informatica.ejb.excepciones;
 
+@SuppressWarnings("serial")
 public class ContrasenaInvalidaException extends TrabajoException{
 
 }
--- src/main/java/es/uma/informatica/ejb/tarea2/GestionLogin.java
+++ src/main/java/es/uma/informatica/ejb/tarea2/GestionLogin.java
@@ -2,15 +2,17 @@ package es.uma.informatica.ejb.tarea2;
 
 import javax.ejb.Local;
 
+import es.uma.informatica.ejb.excepciones.ContrasenaInvalidaException;
 import es.uma.informatica.ejb.excepciones.LoginException;
+import es.uma.informatica.ejb.excepciones.UsuarioInexistenteException;
 import es.uma.informatica.jpa.tarea1.*;
 
 @Local
 public interface GestionLogin {
 	
-	public void login (Login l) throws LoginException;
+	public void login (Login l) throws LoginException, UsuarioInexistenteException, ContrasenaInvalidaException;
 	
-	public Login refrescarLogin (Login l) throws LoginException;
+	public Login refrescarLogin (Login l) throws LoginException, UsuarioInexistenteException, ContrasenaInvalidaException;
 
 	
 	
