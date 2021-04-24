1a31e761a593161d378db5fbd433dface1c0460d
Alejandrodelosriosdiaz <0619861062@uma.es>
2021-04-24 08:12:57 -0700

    encuesta

---
+++ src/main/java/es/uma/informatica/ejb/tarea2/GestionEncuesta.java
@@ -0,0 +1,21 @@
+package es.uma.informatica.ejb.tarea2;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+import es.uma.informatica.ejb.excepciones.TrabajoException;
+import es.uma.informatica.jpa.tarea1.Encuesta;
+//import es.uma.informatica.jpa.tarea1.Login;
+import es.uma.informatica.jpa.tarea1.Solicitud;
+
+@Local
+public interface GestionEncuesta {
+	
+	public void aniadirEncuesta(/*Login login, */Encuesta enc) throws TrabajoException;
+	
+	public void verEncuesta(/*Login login,*/ Encuesta enc) throws TrabajoException;
+	
+	public List<Encuesta> getEncuesta(/*Login login*/) throws TrabajoException;
+	
+}
